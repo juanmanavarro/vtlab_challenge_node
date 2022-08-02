596b962f7c193c35e0e451e0f339472930bb60c4
juanma <juanaco1.p@gmail.com>
2022-08-02 09:31:06 +0200

    login page

---
+++ front/src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="container d-flex align-items-center justify-content-center">
+    <div class="card">
+      <div class="card-header">
+        Login
+      </div>
+      <div class="card-body">
+        <form submit.prevent="login">
+          <input class="form-control mb-2" type="text" placeholder="Username">
+          <input class="form-control mb-2" type="text" placeholder="Password">
+          <button class="btn btn-primary btn-sm float-end" type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container  {
+  height: 100vh;
+}
+</style>
